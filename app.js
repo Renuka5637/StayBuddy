dcc60d681db28f4118122bfe2c44b6873efe8f12
Renuka <renukaachari80@gmail.com>
2025-09-28 15:02:23 +0530

    updated

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const User = require("./models/user.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const listingController = require("../controllers/listings.js");
+const listingController = require("./controllers/listings.js");
 
 
 const dbUrl = process.env.ATLASDB_URL;
